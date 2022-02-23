94c7811e0f58ade3a1ffd2fe96cfa6365db4976c
Elliot Weiss <elliot@Elliots-MacBook-Air.local>
2022-02-23 11:48:13 -0700

    Providing changeset syntax example and explanation of ID nomenclature

--- changelog.sql
+++ changelog.sql
@@ -1,2 +1,17 @@
 --liquibase formatted sql
 
+--changeset Elliot-BAXUS:1
+/*
+
+In order to avoid collision of changesets, we will adopt the following nomenclature:
+
+[Liquibase ID]:[Changeset ID]
+
+Your Liquibase ID will be unique, thus ensuring that you won't conflict with another developer
+It is up to each developer to label each changeset with the correct Changeset ID!
+
+Violators will be thrown in the rancor pit
+
+*/
+
+
