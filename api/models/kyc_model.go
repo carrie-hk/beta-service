123d79f5aeb3e081a165d4bae1120118cc6701df
Carrie Kellar <carriekellar@Carries-MBP.lan>
2022-03-16 10:06:38 -0400

    updated kyc model

--- api/models/kyc_model.go
+++ api/models/kyc_model.go
@@ -1,25 +1,18 @@
 package models
 
 type KYC struct {
-	Wallet          *string `db:"wallet"`
-	FirstName       *string `db:"firstName"`
-	LastName        *string `db:"lastName"`
-	PhoneNumber     *string `db:"phoneNumber"`
-	Email           *string `db:"email"`
-	StreetAddrAShip *string `db:"streetAddrA_ship"`
-	StreetAddrBShip *string `db:"streetAddrB_ship"`
-	CityShip        *string `db:"city_ship"`
-	StateShip       *string `db:"state_ship"`
-	CountryShip     *string `db:"country_ship"`
-	ZipcodeShip     *int64  `db:"zipcode_ship"`
-	StreetAddrABill *string `db:"streetAddrA_bill"`
-	StreetAddrBBill *string `db:"streetAddrB_bill"`
-	CityBill        *string `db:"city_bill"`
-	StateBill       *string `db:"state_bill"`
-	CountryBill     *string `db:"country_bill"`
-	ZipcodeBill     *int32  `db:"zipcode_bill"`
-	BirthDay        *int32  `db:"birthDay"`
-	BirthMonth      *int32  `db:"birthMonth"`
-	BirthYear       *int32  `db:"birthYear"`
-	Title           *string `db:"title"`
+	Wallet_PK    *string `db:"wallet_pk" json:"wallet_pk"`
+	First_Name   *string `db:"first_name" json:"first_name"`
+	Last_Name    *string `db:"last_name" json:"last_name"`
+	Phone_Number *string `db:"phone_num" json:"phone_num"`
+	Email        *string `db:"email" json:"email"`
+	Ship_Addr_A  *string `db:"ship_addr_a" json:"ship_addr_a"`
+	Ship_Addr_B  *string `db:"ship_addr_b" json:"ship_addr_b"`
+	Ship_City    *string `db:"ship_city" json:"ship_city"`
+	Ship_State   *string `db:"ship_state" json:"ship_state"`
+	Ship_ZIP     *int64  `db:"ship_zip" json:"ship_zip"`
+	Dob_Day      *int32  `db:"dob_day" json:"dob_day"`
+	Dob_Month    *int32  `db:"dob_month" json:"dob_month"`
+	Dob_Year     *int32  `db:"dob_year" json:"dob_year"`
+	Title        *string `db:"title" json:"title"`
 }
