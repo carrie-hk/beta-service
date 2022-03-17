980a3a9ffafca6679d9e264581e5639d54eab26b
Elliot Weiss <elliot@Elliots-MacBook-Air.local>
2022-03-17 14:08:18 -0600

    Making redemption queries schema agnostic

--- db/data_access/redemption_access.go
+++ db/data_access/redemption_access.go
@@ -7,7 +7,7 @@ import (
 
 func (db *DbAccess) CreateKYC(kyc models.KYC) error {
 
-	_, err := db.NamedExec(`INSERT INTO baxusnft.kyc (wallet_pk, first_name, last_name, phone_num, email, ship_addr_a, 
+	_, err := db.NamedExec(`INSERT INTO kyc (wallet_pk, first_name, last_name, phone_num, email, ship_addr_a, 
 		ship_addr_b, ship_city, ship_state, ship_zip, dob_day, dob_month, dob_year, title) 
 		VALUES (:wallet_pk, :first_name, :last_name, :phone_num, :email, :ship_addr_a, :ship_addr_b, 
 			:ship_city, :ship_state, :ship_zip, :dob_day, :dob_month, :dob_year, :title)`,
@@ -25,7 +25,7 @@ func (db *DbAccess) CreateKYC(kyc models.KYC) error {
 func (db *DbAccess) GetRedemptionAssets(mintAddr string) ([]models.AssetView, error) {
 	var aa []models.AssetView
 
-	query := "SELECT * from baxusnft.asset_view_table WHERE `mint_addr` = ?"
+	query := "SELECT * from asset_view_table WHERE `mint_addr` = ?"
 
 	err := db.Select(&aa, query, mintAddr)
 	if err != nil {
