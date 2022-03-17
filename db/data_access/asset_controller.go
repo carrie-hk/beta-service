ca4f5bba0d91efc9c3833b2de10b9bf6a0b273c8
Elliot Weiss <elliot@Elliots-MacBook-Air.local>
2022-03-17 14:07:04 -0600

    Making status update query schema agnostic

--- db/data_access/asset_controller.go
+++ db/data_access/asset_controller.go
@@ -7,7 +7,7 @@ import (
 
 func (db *DbAccess) GetAllAssets(pageIndex int, pageSize int) ([]models.AssetView, error) {
 	var aa []models.AssetView
-	query := "SELECT * from baxusnft.asset_view_table WHERE `axu_id` > ? ORDER BY 'axu_id' DESC LIMIT ?"
+	query := "SELECT * from asset_view_table WHERE `axu_id` > ? ORDER BY 'axu_id' DESC LIMIT ?"
 
 	err := db.Select(&aa, query, pageIndex, pageSize)
 	if err != nil {
@@ -20,7 +20,7 @@ func (db *DbAccess) GetAllAssets(pageIndex int, pageSize int) ([]models.AssetVie
 
 func (db *DbAccess) GetFeaturedAssets() ([]models.AssetView, error) {
 	var fa []models.AssetView
-	query := "SELECT * from baxusnft.asset_view_table WHERE `featured`=1"
+	query := "SELECT * from asset_view_table WHERE `featured`=1"
 
 	err := db.Select(&fa, query)
 	if err != nil {
@@ -32,7 +32,7 @@ func (db *DbAccess) GetFeaturedAssets() ([]models.AssetView, error) {
 
 func (db *DbAccess) UpdateAssetStatus(su_list []models.StatusUpdate) error {
 
-	query := "UPDATE baxusnft.axu SET asset_status = :asset_status WHERE axu_id = :axu_id AND mint_addr = :mint_addr"
+	query := "UPDATE axu SET asset_status = :asset_status WHERE axu_id = :axu_id AND mint_addr = :mint_addr"
 
 	for _, su_item := range su_list {
 
