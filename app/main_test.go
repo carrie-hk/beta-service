3789b33c7e9ef7b55348c39061db0365c83ae8e8
Elliot Weiss <elliot@Elliots-MacBook-Air.local>
2022-03-18 15:02:06 -0600

    Removing useless test

--- app/main_test.go
+++ app/main_test.go
@@ -75,35 +75,3 @@ func Test_GetRedemptionAssets(t *testing.T) {
 		t.Errorf("Expected %d, received %d", http.StatusOK, res.StatusCode)
 	}
 }
-
-func Test_GetFeaturedAssets(t *testing.T) {
-	err := godotenv.Load("../.env")
-
-	dbAccess, err := data_access.NewDbAccess()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	assetHandler, err := handlers.NewAssetHandler(dbAccess)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Create test router
-	test_router := gin.New()
-	// Add /asset endpoints to test router
-	router_groups.NewAssetRouter(test_router.Group("/assets"), assetHandler)
-
-	// Create a new server using the test router
-	ts := httptest.NewServer(test_router)
-	defer ts.Close()
-
-	// Ping the "/assets/all" endpoint
-	res, err := http.Get(ts.URL + "/assets/featured")
-	if err != nil {
-		t.Errorf("Expected nil, received %s", err.Error())
-	}
-	if res.StatusCode != http.StatusOK {
-		t.Errorf("Expected %d, received %d", http.StatusOK, res.StatusCode)
-	}
-}
