52d9a599bda9b54f97ff40a9a0f88dd772e590a0
Elliot Weiss <elliot@Elliots-MacBook-Air.local>
2022-03-29 09:05:33 -0600

    Removing requirement for ship_state field, adding validation to ZIP code, updating unit tests

--- api/models/kyc_test.go
+++ api/models/kyc_test.go
@@ -12,6 +12,7 @@ func Test_ValidateKycInfo(t *testing.T) {
 		Ship_Addr_A:  "420 Down-the-Drain Way",
 		Ship_City:    "Pawnee",
 		Ship_State:   "Indiana",
+		Ship_Country: "US&A",
 		Ship_ZIP:     46001,
 		Dob_Day:      20,
 		Dob_Month:    04,
@@ -36,6 +37,7 @@ func Test_MissingFirstNameFails(t *testing.T) {
 		Ship_City:    "Pawnee",
 		Ship_State:   "Indiana",
 		Ship_ZIP:     46001,
+		Ship_Country: "US&A",
 		Dob_Day:      20,
 		Dob_Month:    04,
 		Dob_Year:     1999,
@@ -59,7 +61,56 @@ func Test_IncorrectPhoneNumberFormatFails(t *testing.T) {
 		Ship_Addr_A:  "420 Down-the-Drain Way",
 		Ship_City:    "Pawnee",
 		Ship_State:   "Indiana",
+		Ship_Country: "US&A",
 		Ship_ZIP:     46001,
+		Dob_Day:      20,
+		Dob_Month:    04,
+		Dob_Year:     1999,
+	}
+
+	err := k.Validate()
+
+	if err == nil {
+		t.Fatal(err)
+	}
+}
+
+func Test_TooLongZipFails(t *testing.T) {
+	k := &KYC{
+		Wallet_PK:    "1q2w3e4r5t6y7u8i9o0p9o8i7u6y5t4r",
+		First_Name:   "Janet",
+		Last_Name:    "Snakehole",
+		Phone_Number: "+14204204204",
+		Email:        "janet.snakehole@fireball.ca",
+		Ship_Addr_A:  "420 Down-the-Drain Way",
+		Ship_City:    "Pawnee",
+		Ship_State:   "Indiana",
+		Ship_ZIP:     420420,
+		Ship_Country: "US&A",
+		Dob_Day:      20,
+		Dob_Month:    04,
+		Dob_Year:     1999,
+	}
+
+	err := k.Validate()
+
+	if err == nil {
+		t.Fatal(err)
+	}
+}
+
+func Test_TooShortZipFails(t *testing.T) {
+	k := &KYC{
+		Wallet_PK:    "1q2w3e4r5t6y7u8i9o0p9o8i7u6y5t4r",
+		First_Name:   "Janet",
+		Last_Name:    "Snakehole",
+		Phone_Number: "+14204204204",
+		Email:        "janet.snakehole@fireball.ca",
+		Ship_Addr_A:  "420 Down-the-Drain Way",
+		Ship_City:    "Pawnee",
+		Ship_State:   "Indiana",
+		Ship_Country: "US&A",
+		Ship_ZIP:     0420,
 		Dob_Day:      20,
 		Dob_Month:    04,
 		Dob_Year:     1999,
